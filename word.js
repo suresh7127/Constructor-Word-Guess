86c626ef519cff0a317e9f7ba1ec5e2131aa3d05
suresh <sureshmahato@Sureshs-MacBook-Pro.local>
2019-08-10 06:21:20 -0500

    constructor created for word.js files

--- word.js
+++ word.js
@@ -1,14 +1,14 @@
 var Letter = require("./letter.js");
 
 function Word(answer) {
-  //Letter objects array
+  //create an arrary for letter object
   this.objArray = [];
 
   for (var i = 0; i < answer.length; i++) {
     var letter = new Letter(answer[i]);
     this.objArray.push(letter);
   }
-
+  // create a log function for answerLog
   this.log = function () {
     answerLog = "";
     for (var i = 0; i < this.objArray.length; i++) {
@@ -16,12 +16,12 @@ function Word(answer) {
     }
     console.log(answerLog + "\n========================\n");
   };
-
+  // check guess
   this.userGuess = function (input) {
     for (var i = 0; i < this.objArray.length; i++) {
       this.objArray[i].guess(input);
     }
   };
 }
-
+// export to the word function
 module.exports = Word;
